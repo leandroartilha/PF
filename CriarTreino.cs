ace9c463f342926b28fd68aa9e2b6ac86b2746fe
Leandro Artilha <87547427+leandroartilha@users.noreply.github.com>
2023-12-08 22:40:53 -0300

    Update CriarTreino.cs

--- CriarTreino.cs
+++ CriarTreino.cs
@@ -1,28 +1,28 @@
 public IActionResult CriarTreino(int alunoId, [FromBody] TreinoRequest treinoRequest)
-        {
-            // Verifica se o aluno existe
-            Aluno aluno = _alunos.FirstOrDefault(a => a.Id == alunoId);
-            if (aluno == null)
-            {
-                return NotFound("Aluno não encontrado");
-            }
-
-            // Cria um novo treino
-            Treino treino = new Treino
-            {
-                Nome = treinoRequest.Nome,
-                Exercicios = treinoRequest.Exercicios,
-                Dia = treinoRequest.Dia,
-                Duracao = treinoRequest.Duracao
-            };
-
-            // Atribui o treino ao aluno
-            aluno.AdicionarTreino(treino);
-
-            // Salva o treino na lista de treinos (simulando um "banco de dados")
-            _treinos.Add(treino);
-
-            // Retorna um status de sucesso com o treino criado
-            return Ok(treino);
-        }
+{
+    // Ponto 1: Verifica se o aluno existe
+    Aluno aluno = _alunos.FirstOrDefault(a => a.Id == alunoId);
+    if (aluno == null)
+    {
+        // Ponto 2: Retorna NotFound se o aluno não existir
+        return NotFound("Aluno não encontrado");
     }
+
+    // Ponto 3: Cria um novo treino
+    Treino treino = new Treino
+    {
+        Nome = treinoRequest.Nome,
+        Exercicios = treinoRequest.Exercicios,
+        Dia = treinoRequest.Dia,
+        Duracao = treinoRequest.Duracao
+    };
+
+    // Ponto 4: Atribui o treino ao aluno
+    aluno.AdicionarTreino(treino);
+
+    // Ponto 5: Salva o treino na lista de treinos
+    _treinos.Add(treino);
+
+    // Ponto 6: Retorna Ok com o treino criado
+    return Ok(treino);
+}
